04206168e7f9eb5104e56be742561717318e0342
onekx <1095716578@qq.com>
2020-05-23 12:05:59 +0800

    完成头部组件制作

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './style.css'
 
-const Header = () => {
-    return(
-        <nav className="header">
-            <div className="logo">
-                <a href='http://localhost:3000'>Kx's Blog</a>
-            </div>
-            <ul>
-                <li>
-                    <a href='http://localhost:3000'>首页</a>
-                </li>
-                <li>
-                    <a href='http://localhost:3000'>归档</a>
-                </li>
-                <li>
-                    <a href='http://localhost:3000'>标签</a>
-                </li>
-                <li>
-                    <a href='http://localhost:3000'>关于</a>
-                </li>
-            </ul>
-        </nav>
-    )
+class Header extends Component {
+    state = {
+        showLogo: true,
+        showUl: false,
+        showLine: true
+    }
+
+    switchState = () => {
+        const { showLogo, showUl, showLine } = this.state
+        this.setState({
+            showLogo: !showLogo,
+            showUl: !showUl,
+            showLine: !showLine
+        })
+        document.querySelector('.line').classList.toggle('hidenLine')
+        document.querySelector('.menu').classList.toggle('close')
+        document.querySelector('ul').classList.toggle('showUl')
+    }
+
+    showLogo = () => {
+        const { showLogo } = this.state
+        return showLogo ? 'logo' : 'hidenLogo'
+    }
+
+    render() {
+        return(
+            <nav className="header">
+                <div className={this.showLogo()}>
+                    <a href='http://localhost:3000'>Kx's Blog</a>
+                </div>
+                <div className="menu" onClick={this.switchState}>
+                    <div className="line"></div>
+                </div>
+                <ul>
+                    <li>
+                        <a href='http://localhost:3000'>首页</a>
+                    </li>
+                    <li>
+                        <a href='http://localhost:3000'>归档</a>
+                    </li>
+                    <li>
+                        <a href='http://localhost:3000'>标签</a>
+                    </li>
+                    <li>
+                        <a href='http://localhost:3000'>关于</a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }  
 }
 
 export default Header
