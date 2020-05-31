08c81bef415f4d4f557517cd74a0a83ab928397c
onekx <1095716578@qq.com>
2020-05-31 11:49:11 +0800

    将 class 修改为 hooks

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './style.css'
 
-export default class ArticleList extends Component {
-    render() {
-        return(
-            <div className="article-list">
-                <ul>
-                    <li>
-                        <div className="article">
-                            <h2><a href="localhost:3000">react生命周期</a></h2>
-                            <p>关于react的生命周期关于react的生命周期关于react的生命周期</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+const ArticleList = () => {
+    return(
+        <div className="article-list">
+            <ul>
+                <li>
+                    <div className="article">
+                        <h2><a href="localhost:3000">react生命周期</a></h2>
+                        <p>关于react的生命周期关于react的生命周期关于react的生命周期</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
 }
+
+export default ArticleList
