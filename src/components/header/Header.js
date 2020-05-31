7d05d2b0117bd8dbc093466e67057bf1aa17e80d
kongxin <43380516+onekx@users.noreply.github.com>
2020-05-31 11:50:23 +0800

    将 class 修改为 hooks (#12)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,63 +1,54 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
-class Header extends Component {
-    state = {
+const Header = () => {
+    const [defaultClass, setClass] = useState({
         showLogo: true,
         showUl: false,
         showLine: true
-    }
+    })
 
-    switchState = () => {
-        const { showLogo, showUl, showLine } = this.state
-        this.setState({
-            showLogo: !showLogo,
-            showUl: !showUl,
-            showLine: !showLine
-        })
+    const switchState = () => {
+        setClass(!defaultClass)
         document.querySelector('.line').classList.toggle('hiden-line')
         document.querySelector('.menu').classList.toggle('close')
         document.querySelector('.nav-list').classList.toggle('show-nav')
     }
 
-    showLogo = () => {
-        const { showLogo } = this.state
-        return showLogo ? 'logo' : 'hiden-logo'
+    const changeLogo = () => {
+        return defaultClass.showLogo ? 'logo' : 'hiden-logo'
     }
 
-    render() {
-        return(
-            <header className="header">
-                <div className="header-inner">
-                    <div className={this.showLogo()}>
-                        <span className="logo-text">
-                            <a href='http://localhost:3000'>Kx's Blog</a>
-                        </span>
-                    </div>
-                    <div className="menu" onClick={this.switchState}>
-                        <div className="line"></div>
-                    </div>
-                    <nav className="nav-list">
-                        <ul>
-                            <li className="nav-item">
-                                <a href='http://localhost:3000'>首页</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href='http://localhost:3000'>归档</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href='http://localhost:3000'>标签</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href='http://localhost:3000'>关于</a>
-                            </li>
-                        </ul>
-                    </nav>
+    return(
+        <header className="header">
+            <div className="header-inner">
+                <div className={changeLogo()}>
+                    <span className="logo-text">
+                        <a href='http://localhost:3000'>Kx's Blog</a>
+                    </span>
                 </div>
-                
-            </header>
-        )
-    }  
+                <div className="menu" onClick={()=>switchState()}>
+                    <div className="line"></div>
+                </div>
+                <nav className="nav-list">
+                    <ul>
+                        <li className="nav-item">
+                            <a href='http://localhost:3000'>首页</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href='http://localhost:3000'>归档</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href='http://localhost:3000'>标签</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href='http://localhost:3000'>关于</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>       
+        </header>
+    )
 }
 
 export default Header
