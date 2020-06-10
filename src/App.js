fc82a197e0e6d9d52ac580708bf628e322a1937b
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-10 22:10:43 +0800

    修改样式 (#14)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './components/header/Header'
-import ArticleList from './components/articles/ArticleList'
 
 const App = () => {
   return(
     <div>
       <Header />
-      <ArticleList />
     </div>
 
   )
