9b9f23f9c008d33d4dba23131cbf8269e00f0475
onekx <1095716578@qq.com>
2020-06-12 17:00:58 +0800

    公用底部组件

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Article from './components/article/Article'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return(
     <div>
       <Header />
       <Article />
+      <Footer />
     </div>
   )
 }
