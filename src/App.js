41e1d52daa0fc9e7f1b33c3a209c6f7327f4d826
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-12 17:05:55 +0800

    公用底部组件 (#17)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Article from './components/article/Article'
-import Footer from './components/footer/Footer'
 
 const App = () => {
   return(
     <div>
       <Header />
       <Article />
-      <Footer />
     </div>
   )
 }
