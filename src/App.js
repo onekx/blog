de713e82b34ba573b19995c799d58edd4cde3fa3
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-12 15:04:13 +0800

    主页文章列表布局 (#15)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Article from './components/article/Article'
 
 const App = () => {
   return(
     <div>
       <Header />
+      <Article />
     </div>
-
   )
 }
 
