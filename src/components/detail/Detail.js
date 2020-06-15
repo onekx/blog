ab51d9afd4085f30db927064a016693594bc88f7
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-15 16:30:06 +0800

    使用 marked + highlight 解析文章内容 (#21)

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,5 +1,37 @@
 import React from 'react'
 import './style.css'
+import marked from 'marked'
+import hljs from "highlight.js"
+import 'highlight.js/styles/atelier-forest-light.css'
+
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: true,
+    smartLists: true,
+    smartypants: true,
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value
+    }
+})
+
+let html = marked("### 对Promise的理解:\n" + 
+"js中使用回调函数进行事件处理，如果嵌套多层回调函数，将会陷入 callback hell（回调地狱），使代码难以理解以及维护，例如：\n" +
+"```javascript\n" +
+"method1(function(err, result) {\n    if (err) {\n        throw err;\n    }" +
+"    method2(function(err, result) {\n        if (err) {\n            throw err;\n" +
+"        } \n        method3(function(err, result) {\n            if (err) {\n                throw err;\n" +
+"            } \n            method4(function(err, result) {\n                if (err) {\n" +
+"                    throw err;\n                }\n                method5(result);\n            });\n        });\n" +
+"    });\n});\n```\n" +
+"**then的用法：**\n" +
+"> 引用内容\n" +
+"*倾斜文字*\n" +
+""
+)
 
 const Detail = () => {
     return (
@@ -16,9 +48,7 @@ const Detail = () => {
                         </div>
                     </div>
                 </header>
-                <div>
-                    <p>文章内容</p>
-                </div>
+                <div className="article-content" dangerouslySetInnerHTML={{ __html: html }}></div>
             </div>
         </div>
     )
