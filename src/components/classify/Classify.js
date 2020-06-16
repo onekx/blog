7c08c2d753497d8333823417f0b4712934e9b2ce
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-16 14:15:56 +0800

    归类页面布局 (#24)

---
+++ src/components/classify/Classify.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './style.css'
+
+const Classify = () => {
+    return (
+        <div className="container">
+            <div className="classify-main">
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React-Native</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+                <div className="classify-btn">
+                    <a href="/">React</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Classify
