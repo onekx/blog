1a0391540c6ba1f2eb3030fa00a22b213a9817d7
onekx <1095716578@qq.com>
2020-06-18 20:30:08 +0800

    后台管理架构

---
+++ admin/src/pages/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Card, Input, Button, Spin } from 'antd'
+import { KeyOutlined, UserOutlined } from '@ant-design/icons'
+import 'antd/dist/antd.css'
+import '../static/css/Login.css'
+
+const Login = () => {
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
+    return (
+        <div className="login-div">
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="Kx's Blog System" bordered={true} >
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="输入用户名"
+                        prefix={<UserOutlined />}
+                        onChange={(e) => { setUserName(e.target.value) }}
+                    />
+                    <br /><br />
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="输入密码"
+                        prefix={<KeyOutlined />}
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    />
+                    <br /><br />
+                    <Button type="primary" size="large" block onClick={checkLogin} className="login-btn" > 登录 </Button>
+                </Card>
+            </Spin>
+        </div>
+    )
+}
+
+export default Login
