3c00d86eaec7443e741b9dde442bddb8cffeec09
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-22 20:24:02 +0800

    修改后台管理样式 (#31)

--- admin/src/pages/AdminIndex.js
+++ admin/src/pages/AdminIndex.js
@@ -1,58 +1,39 @@
 import React, { useState } from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd'
-import { PieChartOutlined, DesktopOutlined, UserOutlined, FileOutlined } from '@ant-design/icons'
+import { FileAddOutlined, FileExcelOutlined, FileSyncOutlined } from '@ant-design/icons'
 import '../static/css/AdminIndex.css'
 import { Route } from "react-router-dom"
 import AddArticle from './AddArticle'
 
-const { Header, Content, Footer, Sider } = Layout
-const { SubMenu } = Menu
-
+const { Content, Footer, Sider } = Layout
 
 const AdminIndex = () => {
-    const [collapsed, setCollapsed] = useState(false)
-
-    const onCollapse = collapsed => {
-        setCollapsed(collapsed)
-    }
-
     return (
         <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-                <div className="logo" />
+            <Sider>
+                <div className="logo">
+                    <span>Kx's Blog</span>
+                </div>
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                     <Menu.Item key="1">
-                        <DesktopOutlined />
-                        <span>工作台</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <PieChartOutlined />
+                        <FileAddOutlined />
                         <span>添加文章</span>
                     </Menu.Item>
-                    <SubMenu
-                        key="sub1"
-                        title={
-                            <span>
-                                <UserOutlined />
-                                <span>文章管理</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="3">添加文章</Menu.Item>
-                        <Menu.Item key="4">文章列表</Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="9">
-                        <FileOutlined />
-                        <span>留言管理</span>
+                    <Menu.Item key="2">
+                        <FileSyncOutlined />
+                        <span>修改文章</span>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <FileExcelOutlined />
+                        <span>删除文章</span>
                     </Menu.Item>
                 </Menu>
             </Sider>
             <Layout>
-                <Header style={{ background: '#fff', padding: 0 }} />
                 <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb style={{ margin: '11px 0' }}>
                         <Breadcrumb.Item>后台管理</Breadcrumb.Item>
-                        <Breadcrumb.Item>工作台</Breadcrumb.Item>
+                        <Breadcrumb.Item>添加文章</Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                         <div>
@@ -60,7 +41,7 @@ const AdminIndex = () => {
                         </div>
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>JSPang.com</Footer>
+                <Footer style={{ textAlign: 'center' }}>ONEKX.CN</Footer>
             </Layout>
         </Layout>
     )
