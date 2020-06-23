d21b2ce17f62b334eba77fd9ca9364c21cbe0143
onekx <1095716578@qq.com>
2020-06-23 20:00:27 +0800

    主页展示文章列表

--- blog/src/components/article/Article.js
+++ blog/src/components/article/Article.js
@@ -1,33 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
+import axios from 'axios'
 
 const Article = () => {
+    const [articles, setArticles] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        axios.get('http://localhost:6767/admin/article')
+            .then(res => {
+                setArticles(res.data)
+                setIsLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setIsLoading(false)
+            })
+    }, [])
+
+    // 主页渲染文章
+    const renderArticles = () => {
+        const allArticles = []
+        articles.forEach((val, index) => {
+            allArticles.push(
+                <article key={index} className="article">
+                    <div className="article-body">
+                        <header className="article-title">
+                            <a href="/">{val.title}</a>
+                        </header>
+                        <div className="article-info">
+                            <div className="time">
+                                <time>{val.time}</time>
+                            </div>
+                            <div className="article-category">
+                                <a href="/">{val.tag}</a>
+                            </div>
+                        </div>
+                        <div className="article-content">
+                            <p>{val.desc}</p>
+                        </div>
+                    </div>
+                    <div className="article-footer">
+                        <a href="/detail">阅读全文＞ </a>
+                    </div>
+                </article>
+            )
+        })
+        return allArticles
+    }
+
     return (
         <div className="container">
-            <article className="article">
-                <div className="article-body">
-                    <header className="article-title">
-                        <a href="/">React 生命周期函数</a>
-                    </header>
-                    <div className="article-info">
-                        <div className="time">
-                            <time>2020-6-12</time>
-                        </div>
-                        <div className="article-category">
-                            <a href="/">React</a>
-                        </div>
-                    </div>
-                    <div className="article-content">
-                        <p>属性用于设置多行元素的空间量，如多行文本的间距。
-                        对于块级元素，它指定元素行盒的最小高度。
-                        对于非替代的inline元素，它用于计算行盒的高度
-                        </p>
-                    </div>
-                </div>
-                <div className="article-footer">
-                    <a href="/detail">阅读全文＞ </a>
-                </div>
-            </article>
+            {
+                isLoading
+                    ? (<div>加载中</div>)
+                    : (
+                        <>
+                            {renderArticles()}
+                        </>
+                    )
+            }
         </div>
     )
 }
