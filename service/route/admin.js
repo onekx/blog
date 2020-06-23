b34e80d247aae340d7eed90689baac6470c9f8a0
onekx <1095716578@qq.com>
2020-06-23 16:56:49 +0800

    获取所有文章类别

--- service/route/admin.js
+++ service/route/admin.js
@@ -7,19 +7,18 @@ const admin = express.Router()
 
 const administrator = require('../models/administrator')
 
-admin
-    .post('/admin/login', (req, res) => {
-        const inputName = req.body.name
-        const inputPassword = req.body.password
-        administrator.find((err, doc) => {
-            if (err) console.log(err)
-            else {
-                if (inputName != doc[0].name || inputPassword != doc[0].password)
-                    res.json({ ok: false })
-                else
-                    res.json({ ok: true })
-            }
-        })
+admin.post('/admin/login', (req, res) => {
+    const inputName = req.body.name
+    const inputPassword = req.body.password
+    administrator.find((err, doc) => {
+        if (err) console.log(err)
+        else {
+            if (inputName != doc[0].name || inputPassword != doc[0].password)
+                res.json({ ok: false })
+            else
+                res.json({ ok: true })
+        }
     })
+})
 
 module.exports = admin
